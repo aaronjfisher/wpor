49d10ad9d4c594a63c6ff93a7cf4baaf2764cc9c
f2il-afisher <afisher@foundationmedicine.com>
2023-11-13 05:00:01 +0000

    clean up plots

--- experiments-for-paper/plot_results.R
+++ experiments-for-paper/plot_results.R
@@ -1,64 +1,13 @@
 library(tidyverse)
 library(rslurm)
 
-#type <- 'manual_sbatch'
-type <- 'rslurm'
-if(type == 'rslurm'){
-  sjob <- readRDS('2023-11-03_big_sjob.rds')
-  
-  job_out <- get_slurm_out(sjob, "raw", wait = FALSE)
-  results <- do.call(rbind, job_out[-296])
-  head(results)
-  dim(results)
-}
-job_out[[296]]
-
-setdiff(1:max(results$id), results$id)
-tail(results$id)
-tail(results_full$id)
-grep('Error', results$id)
-sum(is.na(results$learners))
-sum(is.na(results_full$learners))
-
-
-
-if(type == 'manual_sbatch'){
-  job_id <- readLines('latest_sbatch_job_id.txt')
-  slurm_files <- dir('sim_results', full.names = TRUE) %>%
-    as.tibble() %>%
-    filter(grepl(job_id, value)) %>%
-    filter(grepl('.rds', value)) %>%
-    unlist
-  
-  length(slurm_files)
-  
-  results <- lapply(slurm_files, readRDS) %>%
-    do.call(rbind, .) %>%
-    tidyr::unnest(mse)
-}
-
-
-slurm_files <- dir('_rslurm_ivw200_oct26', full.names = TRUE) %>%
-  tibble(value = .) %>%
-  filter(grepl('.RDS', value)) %>%
-  filter(!grepl('/x.RDS', value)) %>%
-  filter(!grepl('/f.RDS', value)) %>%
-  filter(!grepl('/more_args.RDS', value)) %>%
-  unlist
-
-results <- lapply(slurm_files, readRDS) %>%
-  lapply(., rbind) %>%
-  do.call(rbind, .) %>%
-  tidyr::unnest(mse)
-
-
-
-
-#####
+job_path <- '2023-11-08'
+results <- readRDS(paste0(job_path,'_combined_results.rds'))
 
 
 sum_results <- results %>%
-  unnest(mse) %>%
+  filter(!is.na(setup)) %>%
+  tidyr::unnest(mse) %>%
   group_by(
     n_obs, learners, p, 
     sigma, setup, nuisance, 
@@ -66,20 +15,21 @@ sum_results <- results %>%
   summarize(mean_mse = mean(mse),
             se_mse = mean(mse)/sqrt(n()),
             lower_mse = mean_mse - qnorm(0.975) * se_mse,
-            upper_mse = mean_mse + qnorm(0.975) * se_mse)
+            upper_mse = mean_mse + qnorm(0.975) * se_mse) %>%
+  mutate(n_obs = as.numeric(n_obs))
 
 sum_results %>%
   filter(setup == 'A') %>%
   ggplot(aes(x = n_obs, y = mean_mse, col = weights, fill = weights)) +
-  geom_line() + 
+  geom_line(aes(group = weights)) + 
   geom_ribbon(aes(ymin = lower_mse, ymax = upper_mse), alpha = .3, col=NA) +
   facet_grid(pseudo~learners)
 
 sum_results %>%
-  filter(learners == 'boost') %>%
+  filter(learners == 'boost', pseudo != 'rlearner_package') %>%
   ggplot(aes(x = n_obs, y = log(mean_mse), col = weights, fill = weights)) +
-  geom_line() + 
-  geom_ribbon(aes(ymin = log(lower_mse), ymax = log(upper_mse)), alpha = .3, col=NA) +
+  geom_line(aes(group = weights)) + 
+  geom_ribbon(aes(group = weights, ymin = log(lower_mse), ymax = log(upper_mse)), alpha = .3, col=NA) +
   facet_grid(setup~pseudo)
 
 sum_results %>%
@@ -87,7 +37,7 @@ sum_results %>%
          pseudo != 'rlearner_package',
          weights %in% c('weight_1', 'weight_U_AX','weight_DR_X')) %>%
   ggplot(aes(x = n_obs, y = log(mean_mse), col = weights, fill = weights)) +
-  geom_line() + 
+  geom_line(aes(group = weights)) + 
   geom_ribbon(aes(ymin = log(lower_mse), ymax = log(upper_mse)), alpha = .3, col=NA) +
   facet_grid(setup~pseudo)
 
@@ -97,9 +47,9 @@ sum_results %>%
   filter(learners == 'boost',
          pseudo %in% c('pseudo_DR','pseudo_U'),
          weights %in% c('weight_U_AX', 'weight_DR_X')) %>% 
-  ggplot(aes(x = n_obs, y = (mean_mse), col = weights, lty = pseudo, fill = weights)) +
+  ggplot(aes(x = n_obs, y = log(mean_mse), col = weights, lty = pseudo, fill = weights)) +
   geom_line() + 
-  geom_ribbon(aes(ymin = lower_mse, ymax = upper_mse, col = weights), alpha = .3, col = NA) +
+  geom_ribbon(aes(ymin = log(lower_mse), ymax = log(upper_mse), col = weights), alpha = .3, col = NA) +
   facet_grid(setup~.)
 
 
